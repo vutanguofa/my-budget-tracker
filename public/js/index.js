c32eed907e40512441524fa8cd2be80a1a88bd52
Vu Tang <vu.q.tang3@gmail.com>
2020-12-13 12:58:31 -0700

    make it better

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 let transactions = [];
 let myChart;
 
-fetch("/api")
+fetch("/api/transaction")
   .then(response => {
     return response.json();
   })
@@ -113,7 +113,7 @@ function sendTransaction(isAdding) {
   populateTotal();
   
   // also send to server
-  fetch("/api", {
+  fetch("/api/transaction", {
     method: "POST",
     body: JSON.stringify(transaction),
     headers: {
